f00e413f83fbea02a88a24939f62c62e39e3f738
sereta.harmand <sereta.harmand@MacBook-Pro-de-Sereta.local>
2026-05-08 13:31:23 +0200

    [Project config] update components and pages - install sass

---
+++ src/components/Router.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './Layout'
+import Home from '../pages/Home'
+import About from "../pages/About";
+import Logement from "../pages/Logement";
+import Error from "../pages/Error";
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Layout>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/logement" element={<Logement />} />
+                    <Route path="*" element={<Error />} />
+                </Routes>
+            </Layout>
+        </BrowserRouter>
+    )
+}
+
+export default Router
