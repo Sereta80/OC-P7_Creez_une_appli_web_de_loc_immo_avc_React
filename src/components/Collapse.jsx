d208c3fd8cbf638dc0399ed9f0eb8b1d2f7c73dd
sereta.harmand <sereta.harmand@MacBook-Pro-de-Sereta.local>
2026-05-11 22:33:28 +0200

    About page & collapse created

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,1 +1,39 @@
-import { useState } from "react";
+import { useState } from "react"
+import arrow from '../assets/arrow.png'
+import '../styles/Collapse.scss'
+
+
+const Collapse = ({id, title, description}) => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    return isOpen ? (
+        <div className="collapse-opened">
+            <div className="collapse-header">
+                <h2>{title}</h2>
+                <img
+                    src={arrow}
+                    alt="Flèche pointant vers le bas"
+                    className="arrow-down"
+                    onClick={() => setIsOpen(false)}
+                />
+            </div>
+            <div className="collapse-content">
+                <p>{description}</p>
+            </div>
+        </div>
+    ) : (
+        <div className="collapse-closed">
+            <div className="collapse-header">
+                <h2>{title}</h2>
+                <img
+                    src={arrow}
+                    alt="Flèche pointant vers le haut"
+                    className="arrow-up"
+                    onClick={() => setIsOpen(true)}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Collapse
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,19 @@
 import Banner from '../components/Banner'
+import Collapse from '../components/Collapse'
+import aboutContent from '../datas/about-content.json'
 import bannerImage from '../assets/image-about.png'
 
 const About = () => {
     return (
         <>
             <Banner image={bannerImage} alt="Paysage de montagne" />
+            {aboutContent.map((content) => (
+                <Collapse
+                    key={content.id}
+                    title={content.title}
+                    description={content.description}
+                />
+            ))}
         </>
     )
 }
