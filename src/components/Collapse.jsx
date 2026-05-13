99c78f065df311abdd20ba8c182ca958a755d837
sereta.harmand <sereta.harmand@MacBook-Pro-de-Sereta.local>
2026-05-13 07:47:44 +0200

    [Refactoring] Update Collapse component

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -3,35 +3,53 @@ import arrow from '../assets/arrow.png'
 import '../styles/Collapse.scss'
 
 
-const Collapse = ({id, title, description}) => {
+const Collapse = ({title, description}) => {
     const [isOpen, setIsOpen] = useState(false)
 
-    return isOpen ? (
-        <div className="collapse-opened">
-            <div className="collapse-header">
+    // return isOpen ? (
+    //     <div className="collapse-opened">
+    //         <div className="collapse-header">
+    //             <h2>{title}</h2>
+    //             <img
+    //                 src={arrow}
+    //                 alt="Flèche pointant vers le bas"
+    //                 className="arrow-down"
+    //                 onClick={() => setIsOpen(false)}
+    //             />
+    //         </div>
+    //         <div className="collapse-content">
+    //             <p>{description}</p>
+    //         </div>
+    //     </div>
+    // ) : (
+    //     <div className="collapse-closed">
+    //         <div className="collapse-header">
+    //             <h2>{title}</h2>
+    //             <img
+    //                 src={arrow}
+    //                 alt="Flèche pointant vers le haut"
+    //                 className="arrow-up"
+    //                 onClick={() => setIsOpen(true)}
+    //             />
+    //         </div>
+    //     </div>
+    // )
+
+    return (
+        <div className={`collapse-${isOpen ? 'opened' : 'closed'}`}>
+            <div className="collapse-header" onClick={() => setIsOpen(!isOpen)}>
                 <h2>{title}</h2>
                 <img
                     src={arrow}
-                    alt="Flèche pointant vers le bas"
-                    className="arrow-down"
-                    onClick={() => setIsOpen(false)}
+                    alt="flèche"
+                    className={`arrow-${isOpen ? 'down' : 'up'}`}
                 />
             </div>
-            <div className="collapse-content">
-                <p>{description}</p>
-            </div>
-        </div>
-    ) : (
-        <div className="collapse-closed">
-            <div className="collapse-header">
-                <h2>{title}</h2>
-                <img
-                    src={arrow}
-                    alt="Flèche pointant vers le haut"
-                    className="arrow-up"
-                    onClick={() => setIsOpen(true)}
-                />
-            </div>
+            {isOpen && (
+                <div className="collapse-content">
+                    <p>{description}</p>
+                </div>
+            )}
         </div>
     )
 }
--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -12,7 +12,7 @@ const Router = () => {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
-                    <Route path="/logement" element={<Logement />} />
+                    <Route path="/logement/:id" element={<Logement />} />
                     <Route path="*" element={<Error />} />
                 </Routes>
             </Layout>
